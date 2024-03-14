6fe8b9837b000962c1611f184b670453d9eeb8a8
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-03-14 22:20:55 +0000

    added another test.

--- test/hookup_spec.js
+++ test/hookup_spec.js
@@ -5,4 +5,10 @@ describe('2 + 2', () => {
     it('equals 4', () => {
       assert.strictEqual(4, 2 + 2);
     });
-  }); 
+}); 
+
+describe('2 + 0', () => {
+    it('equals 2', () => {
+      assert.strictEqual(2, 2 + 0);
+    });
+})
